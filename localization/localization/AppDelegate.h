de1182d0f65393fcee9e8998455536de181409cc
Burakmeister <35082302+Burakmeister@users.noreply.github.com>
2022-11-29 23:33:40 +0100

    Add files via upload

---
+++ localization/localization/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  localization
+//
+//  Created by Grzegorz on 29/11/2022.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
